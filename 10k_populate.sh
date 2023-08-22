aea5336c38605f78b3ded8afed3087ae30813ad2
Pierce A <pbalworth@gmail.com>
2023-08-22 22:02:08 +0000

    Update 10k_populate.sh

--- 10k_populate.sh
+++ 10k_populate.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
+# generate_random_imsi() {
+#     digits=10
+#     current_time=$(date +%s)
+#     random_number=$((current_time * (1 + RANDOM % 10000)))
+#     while [ ${#random_number} -lt 12 ]; do
+#         random_number="${random_number}$(shuf -i 0-9 -n 1)"
+#     done
+#     imsi_id="${random_number:0:digits}"
+#     echo $imsi_id
+# }
+
 generate_random_imsi() {
-    digits=10
-    current_time=$(date +%s)
-    random_number=$((current_time * (1 + RANDOM % 10000)))
-    while [ ${#random_number} -lt 12 ]; do
-        random_number="${random_number}$(shuf -i 0-9 -n 1)"
-    done
-    imsi_id="${random_number:0:digits}"
+    imsi_id=$(shuf -i 1000000000-9999999999 -n 1)
     echo $imsi_id
 }
 
