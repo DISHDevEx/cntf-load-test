4fa9c351f9b079497b0d73658eafa5f670e35e97
Pierce A <pbalworth@gmail.com>
2023-08-23 15:16:59 +0000

    Update 10k_populate.sh

--- 10k_populate.sh
+++ 10k_populate.sh
@@ -34,8 +34,18 @@ run_helm_commands() {
     $helm_upgrade_command
 }
 
+ue_populate() {
+  imsi_id=$1
+
+  populate_pod_name=$(kubectl -n openverso get pod --output=jsonpath={.items..metadata.name} -l app.kubernetes.io/component=populate)
+
+  if [ $? -eq 0 ]; then
+    kubectl -n openverso exec $populate_pod_name -- open5gs-dbctl add_ue_with_slice $imsi_id 465B5CE8B199B49FAA5F0A2EE238A6BC E8ED289DEBA952E4283B54E88E6183CA internet 1 $imsi_id
+  fi
+}
 for _ in {1..1000}; do
     imsi_id=$(generate_random_imsi)
+    ue_populate
     echo "Subscribing UE with IMSI: ${imsi_id}"
 
     run_helm_commands $imsi_id
